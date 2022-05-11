ab41993eaf0895603985cb7500f441fbdb79a91c
20pa5a1203 <96017135+20pa5a1203@users.noreply.github.com>
2022-05-11 10:53:47 +0530

    Delete Pydi directory

--- Pydi/Snake.java
+++
@@ -1,9 +0,0 @@
-public class Snake{
-    private int head;
-    private int tail;
-    public static List<Integer>snakes=new ArrayList<>();
-    public List<Integer>snakehead=new ArrayList<>();
-    public List<Integer> getsnake(){
-        return this.snakes;
-    }
-}
